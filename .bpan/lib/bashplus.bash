5b6c6a7aad0d494d5148cfee712b9a79fa326d9a
Ingy döt Net <ingy@ingy.net>
2023-01-01 13:31:44 -0500

    Version 0.1.10

--- .bpan/lib/bashplus.bash
+++ .bpan/lib/bashplus.bash
@@ -4,7 +4,7 @@
 # * Many are improved versions of existing builtins/commands.
 
 
-bashplus:version() ( echo '0.1.67' )
+bashplus:version() ( echo '0.1.69' )
 
 bashplus:main() {
   local arg
@@ -30,6 +30,7 @@ bashplus:main() {
 # Source a name like 'foo' or 'foo/bar':
 # shellcheck disable=2086
 +source() {
+  local set=$-; set "${BPAN_X:-+x}"
   local lib=${1?}; shift
 
   local BASHPLUS_PATH=${BASHPLUS_PATH:-$(
@@ -40,6 +41,7 @@ bashplus:main() {
   for path in $(IFS=:; echo ${BASHPLUS_PATH-}); do
     if [[ -f $path/$lib.bash ]]; then
       source "$path/$lib.bash" "$@"
+      [[ $set != *x* ]] || set -x
       return
     fi
   done
@@ -50,6 +52,7 @@ bashplus:main() {
 
 # A simple 'die' function. Full featured version is in lib/bashplus/err.bash
 die() {
+  set "${BPAN_X:-+x}"
   [[ $# -gt 0 ]] || set -- Died
   printf '%s\n' "$@" >&2
   exit 1
@@ -57,12 +60,14 @@ die() {
 
 # A simple 'error' function. Full featured version is in lib/bashplus/err.bash
 error() {
+  set "${BPAN_X:-+x}"
   local msg="Error: ${1:-unknown}"; shift
   die "$msg" "$@"
 }
 
 # Print lines to stderr
 warn() (
+  set "${BPAN_X:-+x}"
   printf '%s\n' "$@" >&2
 )
 
